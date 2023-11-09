128a602ed825e75ecb3c54bc8d599c4c35a93fc8
huangrunhong <huang.run.hong.1986@gmail.com>
2023-11-09 17:00:08 +0100

    first Aufgaben first change

--- Dom-Elements-Level-1_2/asstes/js/main.js
+++ Dom-Elements-Level-1_2/asstes/js/main.js
@@ -1,9 +1,19 @@
+// const styles = document.body.querySelector("body>div");
+// const examples = document.body.querySelectorAll(".example");
+
+// const myFunction = () => {
+//   for (let i = 0; i < examples.length; i++) {
+//     examples[i].classList.toggle("example");
+//   }
+//   styles.classList.toggle("style");
+// };
+
 const styles = document.body.querySelector("body>div");
-const examples = document.body.querySelectorAll(".example");
+const examples = document.body.getElementsByClassName("example");
 
 const myFunction = () => {
   for (let i = 0; i < examples.length; i++) {
-    examples[i].classList.toggle("example");
+    examples[i].classList.toggle("exampleChange");
   }
-  styles.classList.toggle("style");
+  styles.classList.toggle("styleChange");
 };
